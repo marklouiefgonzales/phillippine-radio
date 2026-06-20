9aabb817471a9265ef4dfebd77a7d0ae0d0180d6
marklouiefgonzales <mlfgonzales@yahoo.com>
2026-06-20 08:40:29 +0800

    Update CACHE_VERSION to v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'v25'; // <-- change to whatever value to force devices to download the latest version of the script (just for rare cases that their device maintained old code
+var CACHE_VERSION = 'v26'; // <-- change to whatever value to force devices to download the latest version of the script (just for rare cases that their device maintained old code
 
 self.addEventListener('install', function(event) {
     self.skipWaiting();
